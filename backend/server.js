a7e4c8f002cd8c1251ab507306b252d061f3562a
Janardhan Warang <mobgamingyt0@gmail.com>
2026-02-26 10:48:00 +0530

    cors fixed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dns = require("dns");
 require("dotenv").config();
-
+console.log("ENV CHECK:", process.env.MONGO_URI);
+console.log("PORT CHECK:", process.env.PORT);
+dns.setServers(["8.8.8.8", "1.1.1.1"])
 const app = express();
 
 // Middleware
